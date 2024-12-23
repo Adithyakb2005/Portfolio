0ecfce278e3831288e87be8b2fe92a652f188ca4
Adithyakb2005 <adithyakb1001@gmail.com>
2024-12-24 01:29:52 +0530

    portfolio

--- home.py
+++ home.py
@@ -1,15 +1,13 @@
-from flask import Flask,render_template
-# import sqlite3
-app=Flask(__name__)
-# conn = sqlite3.connect('database.db')
-# try:
-#     conn.execute('''CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT,name TEXT,age INTEGER)''')
-# except:
-#     pass
+from flask import Flask, render_template, send_from_directory
 
-# Home route
-@app.route('/')
+app = Flask(__name__)
+
+@app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
+
+@app.route("/download-resume")
+def download_resume():
+    return send_from_directory(directory=".", path="resume.pdf", as_attachment=True)
 
 app.run()
